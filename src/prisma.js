f61697ad5f61dd96cf6d627535c35820bcefacdf
James Roller, Jr <james.roller@aspencapital.com>
2019-12-10 15:16:53 -0800

    chore: add examples of querying and changing data

--- src/prisma.js
+++ src/prisma.js
@@ -5,14 +5,45 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466',
 });
 
-prisma.query.users(null, '{ id name email posts { id title } }')
-    .then(data => {
-        console.log('users query:');
-        console.log(JSON.stringify(data, null, 2));
-    });
+// prisma.query.users(null, '{ id name email posts { id title } }')
+//     .then(data => {
+//         console.log('users query:');
+//         console.log(JSON.stringify(data, null, 2));
+//     });
 
-prisma.query.comments(null, '{ id text author { id name } post { id title }}')
-    .then(data => {
-        console.log('comments query:');
-        console.log(JSON.stringify(data, null, 2));
-    });
+// prisma.query.comments(null, '{ id text author { id name } post { id title }}')
+//     .then(data => {
+//         console.log('comments query:');
+//         console.log(JSON.stringify(data, null, 2));
+//     });
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: 'GraphQL 101',
+//         body: 'TBD',
+//         published: true,
+//         author: {
+//             connect: {
+//                 id: 'ck40a2rpd007t0930bq2ngh1o'
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then(data => {
+//     console.log(JSON.stringify(data, null, 2));
+//     return prisma.query.users(null, '{ id name email posts { id title } }');
+// }).then(data => {
+//     console.log(JSON.stringify(data, null, 2));
+// });
+
+prisma.mutation.updatePost({
+    where: {
+        id: 'ck40h8wfa00ie09307l6jqi2j'
+    },
+    data: {
+        body: 'Heebee Jeebees'
+    }
+}, null).then(() => {
+    return prisma.query.posts(null, '{ id title body published }');
+}).then(data => {
+    console.log(JSON.stringify(data, null, 2));
+});
