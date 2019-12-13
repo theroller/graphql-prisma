86b8a1cfac0479f84dc3b98e0e538e2f541ede6b
James Roller, Jr <james.roller@aspencapital.com>
2019-12-13 12:20:04 -0800

    feat: users and posts allow for querying

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -9,22 +9,30 @@ const Query = {
         return db.posts[0];
     },
     posts(parent, args, { prisma }, info) {
-        return prisma.query.posts(null, info);
+        const opArgs = {};
+        if (args.query) {
+            opArgs.where = {
+                OR: [
+                    { body_contains: args.query },
+                    { title_contains: args.query }
+                ]
+            };
+        }
 
-        // if (args.query) {
-        //     const re = new RegExp(args.query.toLowerCase());
-        //     return db.posts.filter(x => re.test(x.title.toLowerCase()) || re.test(x.body.toLowerCase()));
-        // }
-        // return db.posts;
+        return prisma.query.posts(opArgs, info);
     },
     users(parent, args, { prisma }, info) {
-        return prisma.query.users(null, info);
+        const opArgs = {};
+        if (args.query) {
+            opArgs.where = {
+                OR: [
+                    { name_contains: args.query },
+                    { email_contains: args.query }
+                ]
+            };
+        }
 
-        // if (args.query) {
-        //     const re = new RegExp(args.query.toLowerCase());
-        //     return db.users.filter(x => re.test(x.name.toLowerCase()));
-        // }
-        // return db.users;
+        return prisma.query.users(opArgs, info);
     },
 };
 
