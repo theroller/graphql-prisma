d1cf3845711a56032a1f1d61484caf9dfb5ba475
James Roller, Jr <james.roller@aspencapital.com>
2019-12-20 17:52:49 -0800

    refactor: move server into separate module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
+'use strict';
+
 require('./env');
-const { GraphQLServer, PubSub } = require('graphql-yoga');
 
-const db = require('./db');
-const { fragmentReplacements, resolvers } = require('./resolvers');
-const prisma = require('./prisma');
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-    context(request) {
-        return {
-            db,
-            pubsub,
-            prisma,
-            request,
-        };
-    },
-    fragmentReplacements,
-    resolvers,
-    typeDefs: './src/schema.graphql',
-});
-
+const server = require('./server');
 const serverConfig = {
     port: process.env.PORT || 4000,
 };
